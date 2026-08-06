1219abe39c36cabd81b4629b1ca9e81f332c8fd2
Venkat Reddy <pandalapadu@gmail.com>
2026-08-06 15:02:50 +0530

    Shell Scripting

--- 3-shell-practice/03-variables.sh
+++ 3-shell-practice/03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-persion1= $1
-persion2= $2
+persion1=$1
+persion2=$2
 echo "$persion1: Hey $persion2, We are going to blast you if you don't listen to us."
 echo "$persion2: Yes please. you are welcome !"
 echo "$persion1: Hey $persion2, I am Serious this time "
