1b8516ecfc41626654601ed8b47c2a59bf5df24e
Venkat Reddy <pandalapadu@gmail.com>
2026-08-06 14:38:16 +0530

    Hello World

---
+++ 3-shell-practice/02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey Iron, We are going to blast you if you don't listen to us."
+echo "Iron: Yes please. you are welcome !"
+echo "Trump: Hey Iron, I am Serious this time "
+echo "Iron: Hey Trump, we are also serious"
