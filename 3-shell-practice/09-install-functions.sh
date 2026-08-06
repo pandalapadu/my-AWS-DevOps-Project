33d49cb82c96f82f4752a975b7bbd65e71a182cf
Venkat Reddy <pandalapadu@gmail.com>
2026-08-06 17:39:02 +0530

    Shell Scripting

--- 3-shell-practice/09-install-functions.sh
+++ 3-shell-practice/09-install-functions.sh
@@ -6,5 +6,12 @@ if [ $USERID -ne 0 ]; then
     echo "You need to have root access to run this script."
     exit 1
 fi
-
 echo "I am continuing the script as root user"
+echo "I am installing mySQL server  on this machine"
+dnf install mysql -y
+if [ $? -eq 0 ]; then
+    echo "mySQL server installed successfully"
+else
+    echo "mySQL server installation failed"
+    exit 1
+fi
