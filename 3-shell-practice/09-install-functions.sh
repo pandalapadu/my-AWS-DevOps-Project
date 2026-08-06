1b99951de15340f0bdf6c1cd6b449bfdfe0b0b8b
Venkat Reddy <pandalapadu@gmail.com>
2026-08-06 17:29:50 +0530

    Shell Scripting

--- 3-shell-practice/09-install-functions.sh
+++ 3-shell-practice/09-install-functions.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 ##check the root access or not 
 if [ $USERID -ne 0 ]; then
     echo "You need to have root access to run this script."
+    exit 1
 fi
 
 echo "I am continuing the script as root user"
