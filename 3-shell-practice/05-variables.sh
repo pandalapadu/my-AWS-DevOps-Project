163b0045bcf1fa132693e2b29d044dbf56e86a0a
Venkat Reddy <pandalapadu@gmail.com>
2026-08-06 15:24:02 +0530

    Shell Scripting

--- 3-shell-practice/05-variables.sh
+++ 3-shell-practice/05-variables.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-TimeStamp=$(date)
-echo "Current Time is: $TimeStamp"
+# TimeStamp=$(date)
+# echo "Current Time is: $TimeStamp"
+
+START_TIME=$(date +%s)
+echo "Start Time is: $START_TIME"
+sleep 10
+END_TIME=$(date +%s)
+echo "End Time is: $END_TIME"
+DIFF_TIME=$((END_TIME - START_TIME))
+echo "Total Time Taken is: $DIFF_TIME seconds"
