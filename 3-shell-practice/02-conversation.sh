7ab75a82af8e620e81834eb50669c6f9b1244c3e
Venkat Reddy <pandalapadu@gmail.com>
2026-08-06 14:44:13 +0530

    Shell Scripting

--- 3-shell-practice/02-conversation.sh
+++ 3-shell-practice/02-conversation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+####################With out Variable ###############
 echo "Trump: Hey Iron, We are going to blast you if you don't listen to us."
 echo "Iron: Yes please. you are welcome !"
 echo "Trump: Hey Iron, I am Serious this time "
 echo "Iron: Hey Trump, we are also serious"
+#####################################################
