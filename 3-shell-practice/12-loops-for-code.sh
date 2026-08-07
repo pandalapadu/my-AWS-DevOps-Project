bacab85ba31a1895ef5e196e12291b2a6d3241f9
Venkat Reddy <pandalapadu@gmail.com>
2026-08-07 10:54:21 +0530

    Shell Scripting

--- 3-shell-practice/12-loops-for-code.sh
+++ 3-shell-practice/12-loops-for-code.sh
@@ -2,27 +2,33 @@
 ##Check root user access or not
 USERID=$(id -u)
 LOG_DIR="/var/log/shell-script"
-LOG_FILE="$LOG_DIR/$(date '+%Y-%m-%d %H:%M:%S').log"
+LOG_FILE="$LOG_DIR/$0.log"
+TIME_STAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
 if [ $USERID -ne 0 ]; then
-    echo "You must be root user to run this script" | tee -a $LOG_FILE
+    echo "[$TIME_STAMP] [ERROR] You must be root user to run this script" | tee -a $LOG_FILE
     exit 1
 fi
-echo "You are root user, you can run this script" | tee -a $LOG_FILE
+echo "[$TIME_STAMP] [INFO] You are root user, you can run this script" | tee -a $LOG_FILE
+
+VALIDATE() {
+    if [ $2 -ne 0 ]; then
+        echo "[$TIME_STAMP] [ERROR] Installing $1 installation failed" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo "[$TIME_STAMP] [SUCCESS] $1 installation Successfully" | tee -a $LOG_FILE
+    fi
+}
+
 
 for package in $@
 do 
    dnf list installed $package &>> $LOG_FILE
    if [ $? -eq 0 ]; then
-       echo "$package is already installed So Skiiping $package installation" | tee -a $LOG_FILE
+       echo "[$TIME_STAMP] [INFO] $package is already installed So Skiiping $package installation" | tee -a $LOG_FILE
    else 
-       echo "$package is not installed So Installing $package" | tee -a $LOG_FILE
+       echo "[$TIME_STAMP] [INFO] $package is not installed So Installing $package" | tee -a $LOG_FILE
        dnf install $package -y &>> $LOG_FILE
-       if [ $? -ne 0 ]; then
-           echo "Installing $package installation failed" | tee -a $LOG_FILE
-           exit 1
-       else
-           echo "$package installation Successfully" | tee -a $LOG_FILE
-       fi
+       VALIDATE "$package" $?
    fi
 done
