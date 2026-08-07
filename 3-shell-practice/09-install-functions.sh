247b75fe20d678027f886482ac2e1cff50c4d8af
Venkat Reddy <pandalapadu@gmail.com>
2026-08-07 08:46:03 +0530

    Shell Scripting

--- 3-shell-practice/09-install-functions.sh
+++ 3-shell-practice/09-install-functions.sh
@@ -7,6 +7,13 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 echo "You are root user, you can run this script"
+##Checking mysql-server is installed or not
+dnf list installed mysql
+if [ $? -eq 0 ]; then
+    echo "mysql-server is already installed"
+    exit 0
+fi
+
 echo "Installing mysql-server"
 dnf install mysql -y 
 if [ $? -ne 0 ]; then
