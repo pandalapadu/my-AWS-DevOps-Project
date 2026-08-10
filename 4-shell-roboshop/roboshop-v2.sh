b2f63499160df87686c897a20a9443629f81bd89
Venkat Reddy <pandalapadu@gmail.com>
2026-08-10 13:56:08 +0530

    Roboshop-Shell

---
+++ 4-shell-roboshop/roboshop-v2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z0580926234LLG39XOC6H"
+DOMAIN_NAME="azdevopsvenkat.site"
+## we have to pass like sh roboshop-v2.sh frontend backend database ... etc 
