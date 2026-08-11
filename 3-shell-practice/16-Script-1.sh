1c14ce44446d0d35b4470f04c691e173b5ccb8b7
Venkat Reddy <pandalapadu@gmail.com>
2026-08-11 10:59:56 +0530

    Script-Calling

--- 3-shell-practice/16-Script-1.sh
+++ 3-shell-practice/16-Script-1.sh
@@ -4,4 +4,5 @@ COUNTRY="india"
 echo "Hello, $COUNTRY!"
 echo "PID of this script-1 is $$"
 
-sh 17-Script-2.sh
+#sh 17-Script-2.sh #this is not calling environment variable of first script
+source ./17-Script-2.sh #this is calling environment variable of first script because source command is used to call the second script and it will run in the same shell as the first script. So, the environment variable COUNTRY will be available in the second script.
