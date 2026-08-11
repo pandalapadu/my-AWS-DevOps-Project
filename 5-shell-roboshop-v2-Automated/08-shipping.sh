168adf05b23c30855a74f9bda371593da8e6e2e4
Venkat Reddy <pandalapadu@gmail.com>
2026-08-11 12:55:33 +0530

    Shell-roboshop-v2

---
+++ 5-shell-roboshop-v2-Automated/08-shipping.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+app_name="shipping"
+source ./common.sh
+CHECK_ROOT_USER
+
+application_setup
+java_setup
+systemd_setup
+
+dnf install mysql -y &>>$LOGS_FILE
+VALIDATE $? "Installing MySQL client"
+
+mysql -h $MYSQL_HOST -u root -pRoboShop@1 -e "use cities" &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+    VALIDATE $? "Data loaded"
+else
+    echo -e "Data already loaded ... $Y SKIPPING $N"
+fi
+
+application_restart
+print_total_time
--- 5-shell-roboshop-v2-Automated/common.sh
+++ 5-shell-roboshop-v2-Automated/common.sh
@@ -83,3 +83,11 @@ application_restart(){
     systemctl restart $app_name &>>$LOGS_FILE
     VALIDATE $? "Restarting $app_name service"
 }
+
+java_setup(){
+    dnf install maven -y &>>$LOGS_FILE
+    VALIDATE $? "Installing Maven"
+    mvn clean package  &>>$LOGS_FILE
+    mv target/shipping-1.0.jar shipping.jar 
+    VALIDATE $? "Installing dependencies"
+}
