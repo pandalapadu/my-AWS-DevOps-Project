8c77a14305e9b692cd16fbee6e983025aad0292a
Venkat Reddy <pandalapadu@gmail.com>
2026-08-11 10:48:04 +0530

    Script-Calling

---
+++ 3-shell-practice/16-Script-1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+COUNTRY="india"
+
+echo "Hello, $COUNTRY!"
+echo "PID of this script-1 is $$"
---
+++ 3-shell-practice/17-Script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Hello, I am in $COUNTRY"
+echo "PID of this script-2 is $$"
