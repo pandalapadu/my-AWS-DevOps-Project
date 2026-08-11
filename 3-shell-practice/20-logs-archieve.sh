d6891ab6938b5c2da9736abfe633005b755ea1cb
Venkat Reddy <pandalapadu@gmail.com>
2026-08-11 16:32:48 +0530

    Shell-practice

--- 3-shell-practice/20-logs-archieve.sh
+++ 3-shell-practice/20-logs-archieve.sh
@@ -2,13 +2,14 @@
 
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14} # Default to 14 days if not provided
+DAYS=${3:-14}
 
 if [ -z "$SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
     echo "Either source directory or destination directory empty"
     echo "USAGE:: $0 [source_dir] [dest_dir] [days:default 14]"
     exit 1
 fi
+
 if [ ! -d "$SOURCE_DIR" ]; then
     echo "Source directory: $SOURCE_DIR does not exist"
     exit 1
@@ -19,7 +20,7 @@ if [ ! -d "$DEST_DIR" ]; then
     exit 1
 fi
 
-FILES=(find "$SOURCE_DIR" -type f -name "*.log" -mtime +$DAYS)
+FILES=$( find "$SOURCE_DIR" -type f -name "*.log" -mtime +$DAYS )
 
 if [ -z "$FILES" ]; then
     echo "Log files older than 14 days not found, nothing to do"
@@ -30,6 +31,7 @@ fi
 # do
 #     echo "$FILE"
 # done <<< "$FILES"
+
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 ARCHIEVE_FILE="$DEST_DIR/logs-archieve-$TIMESTAMP.tar.gz"
 
