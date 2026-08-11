bda3c1907424b6dd156f320f350fd4500e6ed952
Venkat Reddy <pandalapadu@gmail.com>
2026-08-11 14:17:11 +0530

    Shell-practice

--- 3-shell-practice/19-logs-cleanup.sh
+++ 3-shell-practice/19-logs-cleanup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+logs_dir="/var/log/roboshop"
 SOURCE_DIR=$1
 DAYS=${2:-14} #default 14 days if not provided
 
 if [ -z "$SOURCE_DIR" ] || [ -z "$DAYS" ]; then
     echo "Usage: $0 <source_directory> [days(optional, default=14)]"
+    exit 1
+fi
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo "Error: Source directory $SOURCE_DIR does not exist."
     exit 1
 fi
