5d35bdfa6bf4c3ccc26f9e1ea3f21af08d5ec97c
Venkat Reddy <pandalapadu@gmail.com>
2026-08-11 14:30:17 +0530

    Shell-practice

--- 3-shell-practice/19-logs-cleanup.sh
+++ 3-shell-practice/19-logs-cleanup.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
 SOURCE_DIR=$1
-DAYS=${2:-14} #default 14 days if not provided
+DAYS=${2:-14} # default 14 days
 
-if [ -z "$SOURCE_DIR" ] || [ -z "$DAYS" ]; then
-    echo "Usage: $0 <source_directory> [days(optional, default=14)]"
+if [ -z "$SOURCE_DIR" ]; then
+    echo "ERROR:: Missing parameters"
+    echo "USAGE:: $0 <source-dir> [days(optional default to 14)]"
     exit 1
 fi
 
-if [ ! -d $SOURCE_DIR ]; then
-    echo "Error: Source directory $SOURCE_DIR does not exist."
+if [ ! -d "$SOURCE_DIR" ]; then
+    echo "ERROR:: Directory: $SOURCE_DIR does not exist"
     exit 1
 fi
 
-echo "Scanning $SOURCE_DIR for log files older than $DAYS days..."
-Files=(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+echo "Scanning $SOURCE_DIR for log files older than 14 days"
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
-if [ -z "$Files" ]; then
-    echo "No log files older than $DAYS days found in $SOURCE_DIR."
+if [ -z "$FILES" ]; then
+    echo "No log files older than 14 days found"
     exit 0
 fi
-while IFS= read -r FILE; do
-    echo "files are Deleting: $FILE"
-    rm -f "$FILE"
-    echo "Deleted: $FILE"
-done <<< "$Files"
+
+while IFS= read -r FILE
+do
+    echo "File to be deleted: $FILE"
+    rm -f $FILE
+    echo "File $FILE deleted"
+done <<< "$FILES"
+
+# < -> for file
+# <<< -> variable
