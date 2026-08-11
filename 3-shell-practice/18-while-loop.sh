b833709dffa3915df390d01e97b62356df9261d3
Venkat Reddy <pandalapadu@gmail.com>
2026-08-11 13:56:16 +0530

    Shell-practice

--- 3-shell-practice/18-while-loop.sh
+++ 3-shell-practice/18-while-loop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while IFS= read -r line; do  ##Internal Field Separator (IFS) is used to read the line as it is without trimming leading/trailing spaces
     echo "$line"
-done < "$1"
+done < 03-variables.sh
