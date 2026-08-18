1967be1f32c0034f01cbdcac949907607af362da
Venkat Reddy <pandalapadu@gmail.com>
2026-08-18 17:37:44 +0530

    Terraform

--- 5-terraform/04-loops/main.tf
+++ 5-terraform/04-loops/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "roboshop" {
   ami           = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = [
-    aws_security_group.allow_terrafom[count.index].id,
+    aws_security_group.roboshop[count.index].id,
     aws_security_group.common.id
   ] #list
   tags = {
