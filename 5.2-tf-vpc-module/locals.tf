8aef8b37425808450d3665dab5304a2542b9b51e
Venkat Reddy <pandalapadu@gmail.com>
2026-08-21 12:13:47 +0530

    Terraform

--- 5.2-tf-vpc-module/locals.tf
+++ 5.2-tf-vpc-module/locals.tf
@@ -2,7 +2,7 @@ locals {
   common_tags = {
     Project     = var.project
     Environment = var.environment
-    Terraform   = True
+    Terraform   = "True"
     Name        = local.common_name
   }
   common_name = "${var.project}-${var.environment}" #roboshop-dev
--- 5.2-tf-vpc-module/main.tf
+++ 5.2-tf-vpc-module/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "name" {
   cidr_block           = var.vpc_cidr
   instance_tenancy     = "default"
   enable_dns_hostnames = true
-  tags = marge(
+  tags = merge(
     var.vpc_tags,
     local.common_tags
   )
