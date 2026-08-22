68589e5467d9cf0402cc248219c33f583edc5ba4
Venkat Reddy <pandalapadu@gmail.com>
2026-08-22 09:09:41 +0530

    Terraform

--- 5.2-tf-vpc-module/output.tf
+++ 5.2-tf-vpc-module/output.tf
@@ -4,12 +4,12 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-output "public_subnet_id" {
+output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
-output "private_subnet_id" {
+output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
-output "database_subnet_id" {
+output "database_subnet_ids" {
   value = aws_subnet.database[*].id
 }
