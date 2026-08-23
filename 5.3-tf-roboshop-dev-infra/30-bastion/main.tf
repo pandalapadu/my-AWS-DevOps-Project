5a169b8151b5bd077f700a578b442fa681e32bc6
Venkat Reddy <pandalapadu@gmail.com>
2026-08-23 17:03:07 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/30-bastion/main.tf
+++ 5.3-tf-roboshop-dev-infra/30-bastion/main.tf
@@ -3,6 +3,10 @@ resource "aws_instance" "bastion" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id              = local.public_subnet_id
+  user_data = templatefile("${path.module}/bastion.sh.tftpl", {
+    partition_number = 4
+    extend_size = 30
+  })
   # Configuring the root block device
   root_block_device {
     volume_type           = "gp3" # General Purpose SSD (gp2/gp3) or io1/io2
