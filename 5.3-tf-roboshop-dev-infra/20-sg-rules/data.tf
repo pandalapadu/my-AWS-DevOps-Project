3547c1f9061cfbd147f822bfe7740a4404af57da
Venkat Reddy <pandalapadu@gmail.com>
2026-08-23 07:26:24 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/20-sg-rules/data.tf
+++ 5.3-tf-roboshop-dev-infra/20-sg-rules/data.tf
@@ -1,3 +1,20 @@
+###catalogue_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project}/${var.environment}/catalogue_sg_id"
+}
+###mongodb_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name = "/${var.project}/${var.environment}/mongodb_sg_id"
+}
+###catalogue_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project}/${var.environment}/catalogue_sg_id"
+}
+###catalogue_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project}/${var.environment}/catalogue_sg_id"
+}
+###catalogue_sg_id pulling from parameter store 
 data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
---
+++ 5.3-tf-roboshop-dev-infra/20-sg-rules/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+}
