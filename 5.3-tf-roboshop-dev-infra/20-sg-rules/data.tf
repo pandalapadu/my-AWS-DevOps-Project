c7131025a5ee02938e927ea6974f0ba92b586820
Venkat Reddy <pandalapadu@gmail.com>
2026-08-23 07:49:19 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/20-sg-rules/data.tf
+++ 5.3-tf-roboshop-dev-infra/20-sg-rules/data.tf
@@ -1,20 +1,52 @@
-###catalogue_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "catalogue_sg_id" {
-  name = "/${var.project}/${var.environment}/catalogue_sg_id"
-}
 ###mongodb_sg_id pulling from parameter store 
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
-###catalogue_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "catalogue_sg_id" {
-  name = "/${var.project}/${var.environment}/catalogue_sg_id"
+###redis_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "redis_sg_id" {
+  name = "/${var.project}/${var.environment}/redis_sg_id"
+}
+###mysql_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project}/${var.environment}/mysql_sg_id"
+}
+###rabbitmq_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 }
 ###catalogue_sg_id pulling from parameter store 
 data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
-###catalogue_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "catalogue_sg_id" {
-  name = "/${var.project}/${var.environment}/catalogue_sg_id"
+###user_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "user_sg_id" {
+  name = "/${var.project}/${var.environment}/user_sg_id"
+}
+###cart_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "cart_sg_id" {
+  name = "/${var.project}/${var.environment}/cart_sg_id"
+}
+###shipping_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "shipping_sg_id" {
+  name = "/${var.project}/${var.environment}/shipping_sg_id"
+}
+###payment_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "payment_sg_id" {
+  name = "/${var.project}/${var.environment}/payment_sg_id"
+}
+###backend_alb_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "backend_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/backend_alb_sg_id"
+}
+###frontend_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "frontend_sg_id" {
+  name = "/${var.project}/${var.environment}/frontend_sg_id"
+}
+###frontend_alb_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "frontend_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
+}
+###bastion_alb_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "bastion_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/bastion_alb_sg_id"
 }
--- 5.3-tf-roboshop-dev-infra/20-sg-rules/main.tf
+++ 5.3-tf-roboshop-dev-infra/20-sg-rules/main.tf
@@ -1,9 +1,27 @@
-#MongoDB Allowing connections from catalogue service
-resource "aws_security_group_rule" "name" {
+#MongoDB Allowing connections from catalogue service on port 27017
+resource "aws_security_group_rule" "mongodb_catalogue" {
   type = "ingress"
   from_port = 27017
   to_port = 27017
   protocol = "tcp"
-  source_security_group_id = 
-  security_group_id = 
+  source_security_group_id = local.catalogue_sg_id
+  security_group_id = local.mongodb_sg_id
+}
+#MongoDB Allowing connections from user service on port 27017
+resource "aws_security_group_rule" "mongodb_user" {
+  type = "ingress"
+  from_port = 27017
+  to_port = 27017
+  protocol = "tcp"
+  source_security_group_id = local.user_sg_id
+  security_group_id = local.mongodb_sg_id
+}
+#MongoDB Allowing connections from bastion on port 22
+resource "aws_security_group_rule" "mongodb_bastion" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.mongodb_sg_id
 }
