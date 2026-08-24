27e5f99d7759540e5fbb7ef571f4b4a5798fca91
Venkat Reddy <pandalapadu@gmail.com>
2026-08-24 20:11:07 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/40-databases/bootstrap.sh
+++ 5.3-tf-roboshop-dev-infra/40-databases/bootstrap.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-component=$1 #mongodb
-environment=$2 #dev
-dnf install ansible -y
-mkdir -p /var/log/roboshop/
+component=$1
+environment=$2
+
+dnf install ansible git -y
+
+mkdir -p /var/log/roboshop
 chown -R ec2-user:ec2-user /var/log/roboshop
 chmod -R 755 /var/log/roboshop
-touch /var/log/roboshop/ansible.log
 
 cd /home/ec2-user
-git clone https://github.com/pandalapadu/my-AWS-DevOps-Project.git
-cd my-AWS-DevOps-Project/4.2.1-Ansible-roboshop-v3
 
-git pull origin main
+if [ ! -d "my-AWS-DevOps-Project" ]; then
+    git clone https://github.com/pandalapadu/my-AWS-DevOps-Project.git
+else
+    cd my-AWS-DevOps-Project
+    git pull origin main
+    cd ..
+fi
+
+cd /home/ec2-user/my-AWS-DevOps-Project/4.2.1-Ansible-roboshop-v3
+
+ansible-playbook \
+  -e component="$component" \
+  -e env="$environment" \
+  roboshop.yaml
