d77310aa81562f00917e2e2670f3456a6dcf157e
Venkat Reddy <pandalapadu@gmail.com>
2026-08-24 20:53:51 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/40-databases/data.tf
+++ 5.3-tf-roboshop-dev-infra/40-databases/data.tf
@@ -1,19 +1,21 @@
+##MongoDB_Security_Group
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
-
+##redis_Security_Group
 data "aws_ssm_parameter" "redis_sg_id" {
   name = "/${var.project}/${var.environment}/redis_sg_id"
 }
+# ##rabbitmq_Security_Group
+# data "aws_ssm_parameter" "rabbitmq_sg_id" {
+#   name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
+# }
+# ##mySQL_Security_Group
+# data "aws_ssm_parameter" "mysql_sg_id" {
+#   name = "/${var.project}/${var.environment}/mysql_sg_id"
+# }
 
-data "aws_ssm_parameter" "rabbitmq_sg_id" {
-  name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
-}
-
-data "aws_ssm_parameter" "mysql_sg_id" {
-  name = "/${var.project}/${var.environment}/mysql_sg_id"
-}
-
+## All Database Subnets IDs will shown
 data "aws_ssm_parameter" "database_subnet_ids" {
   name = "/${var.project}/${var.environment}/database_subnet_ids"
 }
