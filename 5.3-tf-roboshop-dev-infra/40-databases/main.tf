f3d4c79c54e43d64ce7878c22a05377ecb5397ab
Venkat Reddy <pandalapadu@gmail.com>
2026-08-24 16:38:38 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/40-databases/main.tf
+++ 5.3-tf-roboshop-dev-infra/40-databases/main.tf
@@ -28,7 +28,7 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [ 
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh mongodb ${var.var.environment}"
+      "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
      ]
   }
 }
