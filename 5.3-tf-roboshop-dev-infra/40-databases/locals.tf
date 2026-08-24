d1e325ca4957bd6f09c66647e96187c370169137
Venkat Reddy <pandalapadu@gmail.com>
2026-08-24 17:04:24 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/40-databases/locals.tf
+++ 5.3-tf-roboshop-dev-infra/40-databases/locals.tf
@@ -5,7 +5,7 @@ locals {
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
     common_name = "${var.project}-${var.environment}"
     
-    ami_id = data.aws_ami.joindevops.id
+    ami_id = data.aws_ami.venkat.id
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0] # roboshop-dev-public-1a
     common_tags = {
         Project = "${var.project}"
