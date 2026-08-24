2cb15ca7efc1a91140537cf65c52535b27a11f0a
Venkat Reddy <pandalapadu@gmail.com>
2026-08-24 15:58:42 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/40-databases/main.tf
+++ 5.3-tf-roboshop-dev-infra/40-databases/main.tf
@@ -1,17 +1,27 @@
 resource "aws_instance" "mongodb" {
-  ami                    = local.ami_id
-  instance_type          = "t3.micro"
+  ami           = data.aws_ami.venkat.id
+  instance_type = "t3.micro"
   vpc_security_group_ids = [local.mongodb_sg_id]
-  subnet_id              = local.database_subnet_id
-  user_data = templatefile("${path.module}/bastion.sh.tftpl", {
-    partition_number = 4
-    extend_size = 30
-  })
+  subnet_id = local.database_subnet_id
+  
   tags = merge(
-    local.common_tags,
     {
-      Name = "${local.common_name}-mongodb"
-    }
-
+        Name = "${local.common_name}-mongodb"
+    },
+    local.common_tags
   )
+}
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.mongodb.private_ip
+  }
+  provisioner "remote-exec" {
+    command = 
+  }
 }
