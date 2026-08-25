44940d077bd78b7bb45456e5202c65a6a4709dbd
Venkat Reddy <pandalapadu@gmail.com>
2026-08-25 10:46:32 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/40-databases/data.tf
+++ 5.3-tf-roboshop-dev-infra/40-databases/data.tf
@@ -10,10 +10,10 @@ data "aws_ssm_parameter" "redis_sg_id" {
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
   name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 }
-# ##mySQL_Security_Group
-# data "aws_ssm_parameter" "mysql_sg_id" {
-#   name = "/${var.project}/${var.environment}/mysql_sg_id"
-# }
+##mySQL_Security_Group
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project}/${var.environment}/mysql_sg_id"
+}
 
 ## All Database Subnets IDs will shown
 data "aws_ssm_parameter" "database_subnet_ids" {
