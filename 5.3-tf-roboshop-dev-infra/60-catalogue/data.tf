7a2a48056e489273feb81cdb31435336f60137d5
Venkat Reddy <pandalapadu@gmail.com>
2026-08-26 09:49:25 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/60-catalogue/data.tf
+++ 5.3-tf-roboshop-dev-infra/60-catalogue/data.tf
@@ -13,3 +13,32 @@ data "aws_ssm_parameter" "vpc_id" {
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
     name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
 }
+## AMI ID 
+data "aws_ami" "venkat" {
+  most_recent = true
+  owners      = ["973714476881"] # Official Red Hat AWS Account ID
+
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+output "ami_id" {
+  value = data.aws_ami.venkat.id
+}
--- 5.3-tf-roboshop-dev-infra/60-catalogue/variables.tf
+++ 5.3-tf-roboshop-dev-infra/60-catalogue/variables.tf
@@ -10,9 +10,9 @@ variable "app_version" {
     default = "v3"
 }
 
-/* variable "zone_id" {
-    default = "Z07086101C1CVP7AT2UK4"
-}  */  
+variable "zone_id" {
+    default = "Z0580926234LLG39XOC6H"
+}  
 
 variable "domain_name" {
     default = "azdevopsvenkat.site"
