a3d365ac03c1703c5fa70482f3f7a1b2b77ceb92
Venkat Reddy <pandalapadu@gmail.com>
2026-08-26 09:34:24 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/50-backend-alb/provider.tf
+++ 5.3-tf-roboshop-dev-infra/50-backend-alb/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   # remote state storage location
   backend "s3" {
-    bucket         = "remote-state-90s-dev"
+    bucket         = "azdevopsvenkat.site-dev"
     key            = "roboshop-backend-alb.tfstate"
     region         = "us-east-1"
     encrypt        = true
