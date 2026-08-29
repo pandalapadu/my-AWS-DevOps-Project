a6f0768692a4402f586a2b603495b40af2caacd1
Venkat Reddy <pandalapadu@gmail.com>
2026-08-29 16:56:41 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/50-backend-alb/main.tf
+++ 5.3-tf-roboshop-dev-infra/50-backend-alb/main.tf
@@ -14,3 +14,32 @@ resource "aws_lb" "backend_alb" {
     local.common_tags
   )
 }
+#### Listner creation 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>Hi, i am From Backend ALB from terrafom</h1><p>We will be back online shortly.</p>"
+      status_code  = "200"
+    }
+  }
+}
+
+# Create the Alias Record pointing to the ALB
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id # Your Route 53 Hosted Zone ID
+  name    = "*.backend-alb-${var.environment}-azdevopsvenkat.site"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.backend_alb.dns_name     # Dynamically references ALB DNS name
+    zone_id                = aws_lb.backend_alb.zone_id     # Dynamically references ALB Canonical Zone ID
+    evaluate_target_health = true
+  }
+}
