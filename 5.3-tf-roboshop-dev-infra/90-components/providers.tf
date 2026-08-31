763da44b8bae724e2627a20805152acd409dfb1d
Venkat Reddy <pandalapadu@gmail.com>
2026-08-31 21:22:54 +0530

    Terraform

---
+++ 5.3-tf-roboshop-dev-infra/90-components/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.48.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "azdevopsvenkat.site-dev"    # Replace with your unique S3 bucket name
+    key          = "roboshop-components.tfstate" # Path inside the bucket where the file will sit
+    region       = "us-east-1"                  # Your AWS Region
+    encrypt      = true                         # Encrypts the state file at rest
+    use_lockfile = true                         # Enabiling native state locking file
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
