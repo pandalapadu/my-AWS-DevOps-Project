8b3732bcf27cf3be2a409612d89783470fb3d899
Venkat Reddy <pandalapadu@gmail.com>
2026-08-31 21:14:24 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/tf-roboshop-components/locals.tf
+++ 5.3-tf-roboshop-dev-infra/tf-roboshop-components/locals.tf
@@ -1,12 +1,17 @@
 locals {
     ami_id = data.aws_ami.venkat.id
-    frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
-    common_name = "${var.project}-${var.environment}"
-    public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+    sg_id = data.aws_ssm_parameter.sg_id.value
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    common_name = "${var.project}-${var.environment}-${var.component}"
+     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
+    frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
+    alb_listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
+    host_header = var.component == "frontend" ? "${var.project}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = {
         Project = "${var.project}"
         Environment = "${var.environment}"
         Terraform = "true"
     }
-    certificate_arn = data.aws_ssm_parameter.certificate_arn.value
+    
 }
