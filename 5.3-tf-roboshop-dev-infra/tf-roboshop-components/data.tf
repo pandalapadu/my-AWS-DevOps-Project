fba58fd2816e4e0611fddf5b88e0e567c57f526b
Venkat Reddy <pandalapadu@gmail.com>
2026-08-31 21:12:46 +0530

    Terraform

--- 5.3-tf-roboshop-dev-infra/tf-roboshop-components/data.tf
+++ 5.3-tf-roboshop-dev-infra/tf-roboshop-components/data.tf
@@ -1,14 +1,22 @@
 
-data "aws_ssm_parameter" "frontend_alb_sg_id" {
-    name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
+data "aws_ssm_parameter" "sg_id" {
+    name = "/${var.project}/${var.environment}/${var.component}_sg_id"
 }
 
-data "aws_ssm_parameter" "public_subnet_ids" {
-    name = "/${var.project}/${var.environment}/public_subnet_ids"
+data "aws_ssm_parameter" "private_subnet_ids" {
+    name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
-data "aws_ssm_parameter" "certificate_arn" {
-    name = "/${var.project}/${var.environment}/certificate_arn"
+data "aws_ssm_parameter" "backend_alb_listener_arn" {
+    name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+}
+
+data "aws_ssm_parameter" "frontend_alb_listener_arn" {
+    name = "/${var.project}/${var.environment}/frontend_alb_listener_arn"
+}
+
+data "aws_ssm_parameter" "vpc_id" {
+    name = "/${var.project}/${var.environment}/vpc_id"
 }
 ## AMI ID 
 data "aws_ami" "venkat" {
