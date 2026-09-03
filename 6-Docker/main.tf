ba353389d3d45ad35c72b5fb1a601d7b1a6b4846
Venkat Reddy <pandalapadu@gmail.com>
2026-09-03 16:17:52 +0530

    Docker

--- 6-Docker/main.tf
+++ 6-Docker/main.tf
@@ -23,3 +23,50 @@ resource "aws_instance" "terraform_demo" {
         Environment = "dev"
     }
 }
+# It creates in default VPC
+resource "aws_security_group" "allow_docker" {
+  name        = "allow_terraform"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      =  ["${chomp(data.http.my_public_ip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      =  ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      =  ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 8081
+    to_port          = 8081
+    protocol         = "tcp"
+    cidr_blocks      =  ["0.0.0.0/0"]
+  }
+  
+  # outbound traffic
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" # all traffic
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-docker"
+    Project = "roboshop"
+    Environment = "dev"
+  }
+}
