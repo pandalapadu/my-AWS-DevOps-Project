ace224133ead23a6e591332c95788ed57f8bcb48
Venkat Reddy <pandalapadu@gmail.com>
2026-09-05 16:41:39 +0530

    Kubernetes

--- 7-k8s/00-k8s-infra/eksctl/workstation.sh
+++ 7-k8s/00-k8s-infra/eksctl/workstation.sh
@@ -1,7 +1,24 @@
-# Docker
+################# Docker ################
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
+
+####################### eksctl ###########################
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+sudo curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+sudo tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+###for checking version --> eksctl version
+############### kubectl ##############
+sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.3/2026-04-08/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo cp kubectl /usr/local/bin/kubectl
+
+##for checkingversion # kubectl version --client
