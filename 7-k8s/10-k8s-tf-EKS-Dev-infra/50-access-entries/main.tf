939c23e11875698fdb0c86298c9c0ee0b62004fc
Venkat Reddy <pandalapadu@gmail.com>
2026-09-14 00:49:26 +0530

    K8s-infra-eks

--- 7-k8s/10-k8s-tf-EKS-Dev-infra/50-access-entries/main.tf
+++ 7-k8s/10-k8s-tf-EKS-Dev-infra/50-access-entries/main.tf
@@ -14,19 +14,19 @@ resource "aws_eks_access_policy_association" "bastion" {
   }
 }
 
-# resource "aws_eks_access_entry" "runner" {
-#   cluster_name  = local.eks_cluster_name
-#   principal_arn = local.runner_iam_role_arn
-#   type          = "STANDARD"
-# }
+resource "aws_eks_access_entry" "runner" {
+  cluster_name  = local.eks_cluster_name
+  principal_arn = local.runner_iam_role_arn
+  type          = "STANDARD"
+}
 
-# resource "aws_eks_access_policy_association" "runner" {
-#   cluster_name  = local.eks_cluster_name
-#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-#   principal_arn = local.runner_iam_role_arn
+resource "aws_eks_access_policy_association" "runner" {
+  cluster_name  = local.eks_cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+  principal_arn = local.runner_iam_role_arn
 
-#   access_scope {
-#     type       = "namespace"
-#     namespaces = ["roboshop-dev", "roboshop-sit", "roboshop-uat", "roboshop-prod"]
-#   }
-# }
+  access_scope {
+    type       = "namespace"
+    namespaces = ["roboshop-dev", "roboshop-sit", "roboshop-uat", "roboshop-prod"]
+  }
+}
