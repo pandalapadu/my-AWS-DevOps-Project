3a701c31c53013c12d340a3e2de11a2e40f12050
Venkat Reddy <pandalapadu@gmail.com>
2026-09-13 22:34:43 +0530

    K8s-infra-eks

--- 7-k8s/10-k8s-tf-EKS-Dev-infra/00-vpc/main.tf
+++ 7-k8s/10-k8s-tf-EKS-Dev-infra/00-vpc/main.tf
@@ -2,5 +2,4 @@ module "vpc" {
     source = "git::https://github.com/pandalapadu/my-AWS-DevOps-Project.git//5.1-tf-ec2-instance-module?ref=main"
     project = var.project
     environment = var.environment
-    is_peering_required = false
 }
