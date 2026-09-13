be8eabf0a03db8498ad96f91da7d8ca29cfbbda3
Venkat Reddy <pandalapadu@gmail.com>
2026-09-13 23:24:19 +0530

    K8s-infra-eks

--- 7-k8s/10-k8s-tf-EKS-Dev-infra/10-sgs/variables.tf
+++ 7-k8s/10-k8s-tf-EKS-Dev-infra/10-sgs/variables.tf
@@ -9,7 +9,8 @@ variable "sg_names" {
   default = [
   "mongodb", "redis", "mysql", "rabbitmq", 
   #"catalogue", "user", "cart", "shipping","payment", "backend_alb", "frontend", 
-  "public_alb", "bastion"
-  
+  "public_alb", "bastion",
+  "eks_control_plane",
+  "eks_node"
   ]
 }
