a0e9db7abef7d7831a8dba49033fe0fd9230e4fe
Venkat Reddy <pandalapadu@gmail.com>
2026-09-13 23:32:27 +0530

    K8s-infra-eks

--- 7-k8s/10-k8s-tf-EKS-Dev-infra/20-sg-rules/data.tf
+++ 7-k8s/10-k8s-tf-EKS-Dev-infra/20-sg-rules/data.tf
@@ -14,43 +14,27 @@ data "aws_ssm_parameter" "mysql_sg_id" {
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
   name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 }
-###catalogue_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "catalogue_sg_id" {
-  name = "/${var.project}/${var.environment}/catalogue_sg_id"
-}
-###user_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "user_sg_id" {
-  name = "/${var.project}/${var.environment}/user_sg_id"
-}
-###cart_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "cart_sg_id" {
-  name = "/${var.project}/${var.environment}/cart_sg_id"
-}
-###shipping_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "shipping_sg_id" {
-  name = "/${var.project}/${var.environment}/shipping_sg_id"
-}
-###payment_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "payment_sg_id" {
-  name = "/${var.project}/${var.environment}/payment_sg_id"
-}
-###backend_alb_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "backend_alb_sg_id" {
-  name = "/${var.project}/${var.environment}/backend_alb_sg_id"
-}
-###frontend_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "frontend_sg_id" {
-  name = "/${var.project}/${var.environment}/frontend_sg_id"
-}
-###frontend_alb_sg_id pulling from parameter store 
-data "aws_ssm_parameter" "frontend_alb_sg_id" {
-  name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
+
+###public_alb_sg_id pulling from parameter store 
+data "aws_ssm_parameter" "public_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/public_alb_sg_id"
 }
 
 ###bastio ID
 data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project}/${var.environment}/bastion_sg_id"
 }
+
+###eks_control_plane_sg_id ID
+data "aws_ssm_parameter" "eks_control_plane_sg_id" {
+  name = "/${var.project}/${var.environment}/eks_control_plane_sg_id"
+}
+
+###eks_node ID
+data "aws_ssm_parameter" "eks_node_sg_id" {
+  name = "/${var.project}/${var.environment}/eks_node_sg_id"
+}
+
 ###############getting my laptop Public IP
 data "http" "my_public_ip" {
   url = "https://ipv4.icanhazip.com"
