c1c9d27e46a85fa1341467930dd5f9433c515213
Venkat Reddy <pandalapadu@gmail.com>
2026-09-14 00:41:05 +0530

    K8s-infra-eks

---
+++ 7-k8s/10-k8s-tf-EKS-Dev-infra/50-access-entries/data.tf
@@ -0,0 +1,11 @@
+/* data "aws_ssm_parameter" "eks_cluster_name" {
+    name = "/${var.project}/${var.environment}/eks_cluster_name"
+} */
+
+data "aws_ssm_parameter" "bastion_iam_role_arn" {
+    name = "/${var.project}/${var.environment}/bastion_iam_role_arn"
+}
+
+data "aws_ssm_parameter" "runner_iam_role_arn" {
+    name = "/${var.project}/${var.environment}/runner_iam_role_arn"
+}
