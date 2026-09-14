ec0f73601ee2f9d5cb13eb706f5f30f6c0e4e71d
Venkat Reddy <pandalapadu@gmail.com>
2026-09-14 16:13:39 +0530

    K8s-infra-eks

---
+++ 7-k8s/10-k8s-tf-EKS-Dev-infra/51-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0580926234LLG39XOC6H"
+}  
+
+variable "domain_name" {
+    default = "azdevopsvenkat.site"
+}
