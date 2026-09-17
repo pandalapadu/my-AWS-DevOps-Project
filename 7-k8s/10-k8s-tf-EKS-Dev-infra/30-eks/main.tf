70aa2a52ff54f28a1106acc212dedf21ade6329d
Venkat Reddy <pandalapadu@gmail.com>
2026-09-17 14:02:49 +0530

    K8s-infra-eks

--- 7-k8s/10-k8s-tf-EKS-Dev-infra/30-eks/main.tf
+++ 7-k8s/10-k8s-tf-EKS-Dev-infra/30-eks/main.tf
@@ -63,7 +63,33 @@ module "eks" {
         nodegroup = "blue"
       }
     }
-  }
+    green = {
+      create = var.enable_green
+      kubernetes_version = var.green_version
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.small","t3.medium","m5.xlarge","m4.xlarge"]
+      capacity_type = "SPOT"
 
+      iam_role_additional_policies = {
+        EBS = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicyV2"
+        EFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+      }
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
+
+      # This is required AWS LoadBalancerController
+      metadata_options = {
+        http_endpoint = "enabled"
+        http_put_response_hop_limit = 2
+        http_tokens = "required"
+      }
+
+      labels = {
+        nodegroup = "green"
+      }
+    }
+  }
   tags = local.common_tags
 }
--- 7-k8s/10-k8s-tf-EKS-Dev-infra/30-eks/variables.tf
+++ 7-k8s/10-k8s-tf-EKS-Dev-infra/30-eks/variables.tf
@@ -9,3 +9,18 @@ variable "environment" {
 variable "eks_version" {
   default = "1.36"
 }
+variable "enable_blue" {
+    default = true
+}
+
+variable "blue_version" {
+    default = "1.36"
+}
+
+variable "enable_green" {
+    default = false
+}
+
+variable "green_version" {
+    default = "1.35"
+}
