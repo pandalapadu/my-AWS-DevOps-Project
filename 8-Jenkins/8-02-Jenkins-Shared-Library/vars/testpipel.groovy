54e09b4b571703541a929dc127320672c1f36502
Venkat Reddy <pandalapadu@gmail.com>
2026-09-22 18:07:24 +0530

    Jenkins-catalogue

---
+++ 8-Jenkins/8-02-Jenkins-Shared-Library/vars/testpipel.groovy
@@ -0,0 +1,12 @@
+// jenkins-shared-library/
+// ├── vars/
+// │   ├── nodejsPipeline.groovy     # Encapsulates the entire end-to-end pipeline
+// │   ├── trivyScan.groovy          # Modular reusable step
+// │   └── ecrPush.groovy            # Modular reusable step
+// ├── src/
+// │   └── com/roboshop/Utils.groovy # Standard Groovy classes (optional helper methods)
+// └── resources/                    # Configuration templates, JSON schemas, etc.
+
+def call (){
+
+}
